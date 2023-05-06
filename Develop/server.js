756ad7744ccb898db6f2a3793f3807a601aa18a5
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-05-06 19:54:58 +1000

    added gitigonore and modified server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
 
 const PORT = 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
+const noteDatabase = require ('./db/db.json');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(express.static('public'));
 
-// GET Route for homepage
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) =>
+  res.json(database)
+);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
