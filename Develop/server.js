62f51bda4b547880dfa9a543282383a459556fc3
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-05-06 20:29:17 +1000

    add post routes in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -12,18 +12,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+    console.log(`${req.method} is initiated`);
+});
 
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    console.log(`${req.method} is initiated`);
+});
 
 app.get('/api/notes', (req, res) =>
   res.json(database)
 );
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    updateDb();
+    console.log(`a new note is added with ${new.Note} title`)
+});
 
 
 app.listen(PORT, () =>
